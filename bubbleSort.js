18970b514b59324a3c7009d812bb9f18377ae984
Ming Chen <mzchen14@gmail.com>
2020-03-12 17:16:56 -0400

    bubbleSort and insertionSort

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+
+function bubbleSort(array) {
+    // Write your code here.
+      for (let i = array.length-1; i >= 0; i--){
+          for(let j = 0; j < i; j++){
+              if(array[j] > array[j+1]){
+                  swap(j, j+1, array);
+              }
+          }
+      }
+      return array;
+  }
+  function swap(i, j, array) {
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
---
+++ insertionSort.js
@@ -0,0 +1,26 @@
+
+
+//Approach
+//1. Start by picking second element of arr
+//2 Compare to previous elements, swap if neccessary
+//3. Continue to next element, if it is in the wrong order,
+//iterate through the sorted portion and place.
+//4.Repeat until sorted.
+
+function insertionSort(array) {
+      for(let i = 1; i< array.length; i++) {
+          let current = i;
+          let sorted = i - 1;
+          while(sorted >= 0 && array[current] < array[sorted]) {
+                  swap(current, sorted, array);
+                  current--;
+                  sorted--;
+              }
+      }
+      return array;
+  }
+  const swap = (i, j, array) => {
+      const temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+  }
