bd94b146c5dad44d1a0b410317382881205c9907
Ming Chen <mzchen14@gmail.com>
2020-05-05 22:52:06 -0400

    Edits

--- findRotationPoint.js
+++ findRotationPoint.js
@@ -7,7 +7,7 @@ function findRotationPoint(words) {
     while(left < right) {
       let mid = left + Math.floor(right - left)/2
       if(words[mid] < words[start]) {
-        start = mid
+        start = mid //update the start with the mid if the mid word is "smaller" than the previous word
       }
       if(words[mid] < words[right]) {
         left = mid+1
