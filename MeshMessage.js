879d2b2f01fbf19f9142a1f67f7c4eb89c0eed10
Ming Chen <mzchen14@gmail.com>
2020-05-16 14:15:15 -0400

    Added prompt to Mesh Message

--- MeshMessage.js
+++ MeshMessage.js
@@ -1,4 +1,6 @@
 
+  /* Given information about active users on the network, find the shortest route for a message from one user (the sender) to another (the recipient). Return an array of users that make up this route.
+  */
   // A simple, somewhat inefficient queue implementation
 class Queue {
     constructor() {
