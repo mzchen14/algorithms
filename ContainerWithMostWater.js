122f1d87229eb51a2564e909b8ffdb3d05aff6f4
Ming Chen <mzchen14@gmail.com>
2020-07-23 17:19:31 -0400

    Container with Most Water

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,25 @@
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2. 
+*/
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1
+    let maxArea = 0
+    
+    while(left < right) {
+        let currentArea = Math.min(height[left], height[right]) * (right-left)
+        maxArea = Math.max(maxArea, currentArea)
+        
+        if(height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+    return maxArea
+};
+
+//Time = O(n), Space = O(1)
