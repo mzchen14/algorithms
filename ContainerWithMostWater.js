870754ef099a0cd964a6c1c2981cb9ec1efd031e
Ming Chen <mzchen14@gmail.com>
2020-08-31 15:52:03 -0400

    containsDupe & twoSum python

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -4,22 +4,22 @@ Given n non-negative integers a1, a2, ..., an , where each represents a point at
 Note: You may not slant the container and n is at least 2. 
 */
 
-var maxArea = function(height) {
-    let left = 0;
-    let right = height.length-1
-    let maxArea = 0
-    
-    while(left < right) {
-        let currentArea = Math.min(height[left], height[right]) * (right-left)
-        maxArea = Math.max(maxArea, currentArea)
-        
-        if(height[left] < height[right]) {
-            left++
-        } else {
-            right--
-        }
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    let currentArea = Math.min(height[left], height[right]) * (right - left);
+    maxArea = Math.max(maxArea, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
     }
-    return maxArea
+  }
+  return maxArea;
 };
 
 //Time = O(n), Space = O(1)
--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,39 +1,38 @@
 function twoNumberSum(array, targetSum) {
-      array.sort((a,b) => a-b)
-      let i = 0;
-      let j = array.length-1
-      while(i < j) {
-          if(array[i] + array[j] < targetSum) {
-              i++;
-          } else if(array[i] + array[j] > targetSum) {
-              j--;
-          }
-          else if(array[i] + array[j] === targetSum) {
-              return [array[i], array[j]]
-          }
-      }
-      return [];
+  array.sort((a, b) => a - b);
+  let i = 0;
+  let j = array.length - 1;
+  while (i < j) {
+    if (array[i] + array[j] < targetSum) {
+      i++;
+    } else if (array[i] + array[j] > targetSum) {
+      j--;
+    } else if (array[i] + array[j] === targetSum) {
+      return [array[i], array[j]];
+    }
   }
-  
-  //Time: O(n log n) Space: O(1)
+  return [];
+}
+
+//Time: O(n log n) Space: O(1)
 
 /* 7/13 Review */
 function twoNumberSum(array, targetSum) {
-    // Write your code here.
-      let set = new Set()
-      for(let i = 0; i < array.length; i++) {
-          if(!set.has(array[i])) {
-              set.add(array[i])
-          }
-      }
-      
-      for(let i = 0; i < array.length; i++) {
-          if(set.has(targetSum-array[i]) && targetSum-array[i] !== array[i]) {
-              return [array[i], targetSum-array[i]]
-          }
-      }
-      return []
+  // Write your code here.
+  let set = new Set();
+  for (let i = 0; i < array.length; i++) {
+    if (!set.has(array[i])) {
+      set.add(array[i]);
+    }
   }
+
+  for (let i = 0; i < array.length; i++) {
+    if (set.has(targetSum - array[i]) && targetSum - array[i] !== array[i]) {
+      return [array[i], targetSum - array[i]];
+    }
+  }
+  return [];
+}
 
 /* This is a naive solution. It uses a set to track the current numbers in the array, 
 and then on the second loop, it searches for the second number.
@@ -45,16 +44,3 @@ Once sorted, binary search is always an option.
 NOTE: This quetion is different from Two Sum on Leetcode which asks you to return the INDICES,
 in that case, sorting would not be an option.
 */
-
-/* Python Solution */
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, value in enumerate(nums):
-            remaining = target - nums[i]
-            
-            if remaining in seen:
-                return [i, seen[remaining]]
-            else:
-                seen[value] = i
